1c53b104848bb3829abdf260ec80b49fd0193a2b
Nicolas Sauveur <nicolas.sauveur@gmail.com>
2020-10-01 16:26:11 +0200

    first commit

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function homepage()
+    {
+        return $this->render('default/homepage.html.twig', [
+        ]);
+    }
+}
